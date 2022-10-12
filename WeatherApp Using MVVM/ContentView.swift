5f092ceeb4dbcb4610982c16d15078a85907041b
Prraneth-Kumar <prranethkumar22@gmail.com>
2022-10-12 16:11:48 +0530

    Initial Commit

---
+++ WeatherApp Using MVVM/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeatherApp Using MVVM
+//
+//  Created by Prraneth Kumar A R on 12/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp Using MVVM/WeatherApp_Using_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_Using_MVVMApp.swift
+//  WeatherApp Using MVVM
+//
+//  Created by Prraneth Kumar A R on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_Using_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
