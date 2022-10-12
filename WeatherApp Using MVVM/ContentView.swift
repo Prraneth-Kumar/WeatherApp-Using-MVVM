f2145e631d28e9117ccc03748f7dcff2d4345c9f
Prraneth-Kumar <prranethkumar22@gmail.com>
2022-10-12 16:14:43 +0530

    first commit

--- WeatherApp Using MVVM/ContentView.swift
+++ WeatherApp Using MVVM/ContentView.swift
@@ -6,18 +6,59 @@
 //
 
 import SwiftUI
+import Alamofire
+import SwiftyJSON
 
 struct ContentView: View {
+    
+    @State var cityName = ""
+    
+    @StateObject var viewMode = ViewModel()
+    let  colorTitle = RadialGradient(gradient: Gradient(colors: [.gray, .gray, .orange]), center: .center, startRadius: 430, endRadius: 30)
+    let color1 = LinearGradient(gradient: Gradient(colors: [.gray, .blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.white, .orange]), startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                VStack{
+                    Text("Enter City Name For Weather")
+                    TextField("Enter City or State", text: $cityName)
+                        .multilineTextAlignment(.center)
+                    Button("Search"){
+                        viewMode.cityName = cityName
+                        viewMode.value1()
+                    }.font(.system(size: 20, weight: .semibold))
+                    List($viewMode.storeInArray) {$data in
+                        Text("\(data.name)").fontWeight(.bold).multilineTextAlignment(.center)
+                           .listRowBackground(colorTitle)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                        HStack{
+                            VStack{
+                                Image("\(data.image)")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: 100, maxHeight: 100)
+                            }
+                            VStack{
+                                Text("Temp: \(data.temp, specifier: "%.2f")°C").fontWeight(.bold)
+                                Text("Clouds: \(data.clouds)%").fontWeight(.bold)
+                                Text("Humidity: \(data.humidity)%").fontWeight(.bold)
+                                Text("Speed: \(data.speed, specifier: "%.2f")m/s").fontWeight(.bold)
+                                Text("Pressure: \(data.pressure)hpa").fontWeight(.bold)
+                            }
+                        }.listRowBackground(color1)
+                    }.listStyle(.grouped)
+                       // .background(colorTitle)
+                        .scrollContentBackground(.hidden)
+                }
+            }
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
